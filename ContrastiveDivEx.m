80191a3db59e8edb9f5f7f929f00bd79f4d86066
pkjkrai66 <pankajrai87@gmail.com>
2017-03-17 21:53:40 +0530

    adding files

---
+++ ContrastiveDivEx.m
@@ -0,0 +1,4 @@
+% This is just a demonstration for Contrastive Divergence
+% Contrastive Divergence is used in Product of Expert in order to estimate
+% the parameters of models(also called as experts)
+
