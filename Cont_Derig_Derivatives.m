3e39d170fe699a4369f423d8e4504a64d90228ac
pkjkrai66 <pankajrai87@gmail.com>
2017-03-21 15:56:01 +0530

    .asv file removed

---
+++ Cont_Derig_Derivatives.m
@@ -0,0 +1,15 @@
+function derivatives_wrtParam = Cont_Derig_Derivatives(x, param0)
+
+
+mu1_guess = param0(1);
+mu2_guess = param0(2);
+sig1_guess = param0(3);
+sig2_guess = param0(4);
+
+delg_mu1 = (x - mu1_guess)/sig1_guess^2;
+delg_mu2 = (x - mu2_guess)/sig2_guess^2;
+
+delg_sig1 = (x - mu1_guess)^2/sig1_guess^3;
+delg_sig2 = (x - mu2_guess)^2/sig2_guess^3;
+
+derivatives_wrtParam = [delg_mu1 delg_mu2 delg_sig1 delg_sig2];
--- ContrastiveDivEx.m
+++ ContrastiveDivEx.m
@@ -3,6 +3,7 @@
 % the parameters of models(also called as experts)
 
 % Considering two gaussian distributions with mean and std as follows
+clear; close all;
 mu1 = 1;
 sig1 = 1.5;
 mu2 = 3;
@@ -11,42 +12,53 @@ sig2 = 3;
 % A third distribution which is product of the two distributions is
 % PROPTIONAL to Gaussian with mean and stdev given by following relation 
 
-mu = (mu1*sig2^2 + mu2*sig1^2)/(sig1^2 + sig2^2);
-sig = (sig1^2 * sig2^2)/(sig1^2 + sig2^2);
+mu_R = (mu1*sig2^2 + mu2*sig1^2)/(sig1^2 + sig2^2);
+sig_R = (sig1^2 * sig2^2)/(sig1^2 + sig2^2);
 
 % ~~~~~~~~~~~ Plotting ~~~~~~~~~~~~~~~
-x = -4:.1:6;
-pdf1 = normpdf(x, mu1, sig1);
-pdf2 = normpdf(x, mu2, sig2);
+t = -4:.1:6;
+pdf1 = normpdf(t, mu1, sig1);
+pdf2 = normpdf(t, mu2, sig2);
 
 figure;
-plot(x, pdf1, 'r'); hold on
-plot(x, pdf2, 'g') 
+plot(t, pdf1, 'r'); hold on
+plot(t, pdf2, 'g') 
 legend({'Distribution1', 'Distribution2'})
 
-pdfR = normpdf(x, mu, sig);
+pdfR = normpdf(t, mu_R, sig_R);
 figure;
-plot(x, pdf, 'k');
+plot(t, pdfR, 'k');
 legend('Dist1*Dist2')
+
 % -x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-
 
-X = normrnd(mu, sig, [1000,1]);
-
-delg_mu1 = (XX - mu1_guess)/sig1_guess^2;
-delg_mu2 = (XX - mu2_guess)/sig2_guess^2;
-
-delg_sig1 = (XX - mu1_guess)^2/sig1_guess^3;
-delg_sig2 = (XX - mu2_guess)^2/sig2_guess^3;
-
+X = normrnd(mu_R, sig_R, [1000,1]);
 
 % parameters = [mean1 mean2 sigma1 sigma2]
 
-param = [3.5 5 2.5 4.5];
+param0 = [3.5, 5, 2.5, 4.5];
+deltaParam = 0.005;
+param = param0;
 
+for i = 1:100
+    deriv_wrtParam = Cont_Derig_Derivatives(X, param(i,:));
 
-
-
-
-
+    jumpin_sigma = .05;
+    Xprime = normrnd(X, jumpin_sigma);
+    
+    % M-H sampling
+    
+    logliklihood_X = sum(-(X-param(1)).^2/param(3)^2)+sum(-(X-param(2)).^2/param(4)^2);
+    logliklihood_Xprime = sum(-(Xprime-param(1)).^2/param(3)^2)+sum(-(Xprime-param(2)).^2/param(4)^2);
+    
+    
+    CompareLikli = logliklihood_Xprime - logliklihood_X;
+    f = min(0, CompareLikli);
+    r = rand;
+    if f>r
+        deriv_wrtParamPrime = Cont_Derig_Derivatives(Xprime, param(i,:));
+        param(i+1,:) = param(i) + deltaParam*(deriv_wrtParam-deriv_wrtParamPrime);
+    end
+end
 
 
